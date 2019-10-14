10be67541ff9b5b9480554f4f35e9cc33534b640
Lucas Rademaker <lr4d@users.noreply.github.com>
2019-10-14 14:34:07 +0200

    separate build from main test part

--- before_install.sh
+++ before_install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Build base hadoop image
 docker build -t hadoop-base hadoop
+# Build rest of images (done here instead of with `docker-compose up --build` to keep test output clean)
+docker-compose build
